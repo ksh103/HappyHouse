f7f30a88ace6cb7ba2fe969d8fbfba461ac89d18
Dae-Eon, Park <mem29238@gmail.com>
2021-11-18 00:31:18 +0900

    로그인, 회원가입 수정

--- client/src/components/user/Join.vue
+++ client/src/components/user/Join.vue
@@ -11,35 +11,35 @@
         <div class="col-12">
           <div class="mb-2">
             <label class="form-label">이름</label>
-            <input type="name" class="form-control" placeholder="이름을 입력하세요." />
+            <input v-model="userName" type="name" class="form-control" placeholder="이름을 입력하세요." />
           </div>
         </div>
         <div class="col-12">
           <div class="mb-2">
             <label class="form-label">아이디</label>
-            <input type="id" class="form-control" placeholder="아이디를 입력하세요." />
-          </div>
-        </div>
-        <div class="col-12">
-          <div class="mb-2">
-            <label class="form-label">이메일</label>
-            <input type="email" class="form-control" placeholder="아이디를 입력하세요." />
+            <input v-model="userId" type="id" class="form-control" placeholder="아이디를 입력하세요." />
           </div>
         </div>
         <div class="col-12">
           <div class="mb-2">
             <label class="form-label">비밀번호</label>
-            <input type="password" class="form-control" placeholder="비밀번호를 입력하세요."/>
+            <input v-model="userPassword" type="password" class="form-control" placeholder="비밀번호를 입력하세요."/>
           </div>
         </div>
         <div class="col-12">
           <div class="mb-2">
             <label class="form-label">비밀번호 확인</label>
-            <input type="password" class="form-control" placeholder="비밀번호를 다시 입력하세요." />
+            <input v-model="userRePassword" type="password" class="form-control" placeholder="비밀번호를 다시 입력하세요." />
+          </div>
+        </div>
+        <div class="col-12">
+          <div class="mb-2">
+            <label class="form-label">이메일</label>
+            <input v-model="userEmail" type="email" class="form-control" placeholder="이메일을 입력하세요." />
           </div>
         </div>
         <div class="col-12 text-center mt-4">
-          <a href="index.html" class="btn btn-dark lift text-uppercase">join us</a>
+          <a @click="join" href="#" class="btn btn-dark lift text-uppercase">join us</a>
         </div>
         <div class="col-12 text-center mt-4">
           <span class="text-muted">이미 계정이 존재하시나요?<router-link class="nav-link" to="/user/login">login</router-link></span>
@@ -53,7 +53,52 @@
 </template>
 
 <script>
-export default {};
+import http from "@/common/axios.js";
+import Vue from 'vue';
+import VueAlertify from 'vue-alertify';
+import { mapActions } from 'vuex';
+
+Vue.use(VueAlertify);
+
+export default {
+  name: 'Join',
+  data() {
+    return {
+      userId: '',
+      userPassword: '',
+      userRePassword: '',
+      userName: '',
+      userEmail: '',
+    }
+  },
+  methods: {
+    ...mapActions(['login']),
+    join() {
+      // alert('hi')
+      http.post('/user', {
+          userName: this.userName,
+          userId: this.userId,
+          userPassword: this.userPassword,
+          userEmail: this.userEmail,
+        })
+        .then(response => {
+          let $this = this;
+          if (response.result == 1) {
+            this.$alertify.alert('가입이 완료되었습니다. 로그인 페이지로 이동합니다.', function() {
+              $this.$router.push('/user/login/');
+            });
+          }
+        })
+        .catch(error => {
+          console.log("RegisterVue: error : ");
+          console.log(error);
+          if( error.response.status == '404'){
+            this.$alertify.error('Opps!! 서버에 문제가 발생했습니다.');          
+          }
+        })
+    }
+  }
+};
 </script>
 
 <style></style>
--- client/src/components/user/Login.vue
+++ client/src/components/user/Login.vue
@@ -1,52 +1,68 @@
 <template>
-  <!-- Main Body -->
-    <div class="container">
-        <div class="card col-md-5 mt-5" style="float: none; margin:0 auto;">
-        <!-- Form -->
-        <form class="row g-1 p-0 p-md-4">
-            <div class="col-12 text-center mt-5" >
-                <h1>LOGIN</h1>
-            </div>
-            <div class="col-12 mt-5">
-                <div class="mb-3">
-                    <label class="form-label">아이디</label>
-                    <input type="email" class="form-control" placeholder="name@example.com">
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="mb-3">
-                    <div class="form-label">
-                        <span class="d-flex justify-content-between align-items-center"> 
-                        비밀번호
-                        <a class="text-primary" href="auth-password-reset.html">비밀번호 찾기</a>
-                        </span>
-                    </div>
-                    <input type="password" class="form-control" placeholder="***************">
-                </div>
-            </div>
-            <div class="col-12">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-                    <label class="form-check-label" for="flexCheckDefault">
-                        아이디 저장
-                    </label>
-                </div>
-            </div>
-            <div class="col-12 text-center mt-4 mb-3">
-                <a href="index.html" class="btn btn-dark">login</a>
-            </div>
-            <div class="col-12 text-center mt-4">
-                <span class="text-muted">회원가입 시 다양한 주거지 실거래가 정보를 제공합니다! <router-link class="nav-link" to="/user/join">join us</router-link></span>
-            </div>
-        </form>
-        <!-- End Form -->
-        </div>
-    </div> <!-- End Row -->
+  <div class="container">
+    <div class="card col-md-5 mt-5" style="float: none; margin:0 auto;">
+      <form class="row g-1 p-0 p-md-4">
+          <div class="col-12 text-center mt-5" >
+              <h1>LOGIN</h1>
+          </div>
+          <div class="col-12 mt-5">
+              <div class="mb-3">
+                  <label class="form-label">아이디</label>
+                  <input v-model="userId" type="email" class="form-control" placeholder="name@example.com">
+              </div>
+          </div>
+          <div class="col-12">
+              <div class="mb-3">
+                  <div class="form-label">
+                      <span class="d-flex justify-content-between align-items-center"> 
+                      비밀번호
+                      <a class="text-primary" href="auth-password-reset.html">비밀번호 찾기</a>
+                      </span>
+                  </div>
+                  <input v-model="userPassword" type="password" class="form-control" placeholder="***************">
+              </div>
+          </div>
+          <div class="col-12">
+              <div class="form-check">
+                  <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                  <label class="form-check-label" for="flexCheckDefault">
+                      아이디 저장
+                  </label>
+              </div>
+          </div>
+          <div class="col-12 text-center mt-4 mb-3">
+              <a @click="validateForm" href="#" class="btn btn-dark">login</a>
+          </div>
+          <div class="col-12 text-center mt-4">
+              <span class="text-muted">회원가입 시 다양한 주거지 실거래가 정보를 제공합니다! <router-link class="nav-link" to="/user/join">join us</router-link></span>
+          </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
+import { mapActions } from 'vuex';
 
+export default {
+  name: 'Login',
+  data() {
+    return {
+      userId: '',
+      userPassword: '',
+    }
+  },
+  methods: {
+    ...mapActions(['login']),
+    validateForm() {
+      // TODO : id, password validation
+      alert('validate form 후 로그인 호출')
+      this.login({
+        userId: this.userId,
+        userPassword: this.userPassword
+      });
+    }
+  }
 }
 </script>
 
