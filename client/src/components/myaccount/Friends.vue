b419b93b5bbf43fb98f0c24bf6b717316134a202
Dae-Eon, Park <mem29238@gmail.com>
2021-11-23 13:44:38 +0900

    마이페이지 - 친구

--- client/src/components/myaccount/Friends.vue
+++ client/src/components/myaccount/Friends.vue
@@ -8,9 +8,29 @@
             <button style="font-size: 14px;" class="btn btn-primary mt-4 mb-4 px-2 py-1 lift float-end">친구 추가</button>
           </div>
           <div class="row row-cols-xl-6 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-2 g-1 mb-4 row-deck">
-            <div v-for="(item, index) in items" :key="index" class="col">
+            <div v-for="(item, index) in followingList" :key="index" class="col">
               <div class="card text-center">
                   <!-- Group Hover action -->
+                <div class="btn-group position-absolute top-0 right-0">
+                  <a href="#" class="nav-link py-2 px-3 text-muted" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
+                  <ul class="dropdown-menu dropdown-menu-end border-0 shadow">
+                    <li><a class="dropdown-item" href="#">관심 매물 보기</a></li>
+                    <li><a class="dropdown-item" href="#">삭제</a></li>
+                  </ul>
+                </div>
+                <div class="card-body d-flex align-items-center justify-content-between flex-column">
+                  <div class="me-auto ms-auto py-4">
+                    <img src="../../assets/images/profile_av.png" alt="" class="rounded-circle">
+                  </div>
+                  <div class="mt-2">
+                      <h6 class="mb-0">{{ item.userName }}</h6>
+                      <small class="text-muted">{{ item.userEmail }}</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-for="(item, index) in items" :key="index+99" class="col">
+              <div class="card text-center">
                 <div class="btn-group position-absolute top-0 right-0">
                   <a href="#" class="nav-link py-2 px-3 text-muted" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
                   <ul class="dropdown-menu dropdown-menu-end border-0 shadow">
@@ -29,37 +49,17 @@
                 </div>
               </div>
             </div>
-            <!-- <div v-for="(item, index) in items" :key="index" class="col">
-              <div class="card text-center">
-                <div class="btn-group position-absolute top-0 right-0">
-                  <a href="#" class="nav-link py-2 px-3 text-muted" data-bs-toggle="dropdown" aria-expanded="false"><i class="fa fa-ellipsis-v"></i></a>
-                  <ul class="dropdown-menu dropdown-menu-end border-0 shadow">
-                    <li><a class="dropdown-item" href="#">관심 매물 보기</a></li>
-                    <li><a class="dropdown-item" href="#">삭제</a></li>
-                  </ul>
-                </div>                                            
-                <div class="card-body d-flex align-items-center justify-content-between flex-column">
-                  <div class="me-auto ms-auto py-4">
-                    <img src="../../assets/images/profile_av.png" alt="" class="rounded-circle">
-                  </div>
-                  <div class="mt-2">
-                      <h6 class="mb-0">김개똥</h6>
-                      <small class="text-muted">ssafy@ssafy.com</small>
-                  </div>
-                </div>
-              </div>
-            </div> -->
           </div> <!-- .row end-->
         </div>
       </div> <!-- .row end-->
       <div class="row">
         <div class="col-12">
-          <div class="d-flex justify-content-between align-items-center">
+          <!-- <div class="d-flex justify-content-between align-items-center"> -->
             <h1 class="h3 m-4">나의 친구</h1>
-            <button style="font-size: 14px;" class="btn btn-primary mt-4 mb-4 px-2 py-1 lift float-end">친구 추가</button>
-          </div>
+            <!-- <button style="font-size: 14px;" class="btn btn-primary mt-4 mb-4 px-2 py-1 lift float-end">친구 추가</button> -->
+          <!-- </div> -->
           <div class="row row-cols-xl-6 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-2 g-1 mb-4 row-deck">
-            <div v-for="(item, index) in items2" :key="index" class="col">
+            <div v-for="(item, index) in followerList" :key="index+50" class="col">
               <div class="card text-center">
                   <!-- Group Hover action -->
                 <div class="btn-group position-absolute top-0 right-0">
@@ -74,8 +74,8 @@
                     <img src="../../assets/images/profile_av.png" alt="" class="rounded-circle">
                   </div>
                   <div class="mt-2">
-                      <h6 class="mb-0">김개똥</h6>
-                      <small class="text-muted">ssafy@ssafy.com</small>
+                      <h6 class="mb-0">{{ item.userName }}</h6>
+                      <small class="text-muted">{{ item.userEmail }}</small>
                   </div>
                 </div>
               </div>
@@ -95,7 +95,7 @@ export default {
   data() {
     return {
       items: [1, 2, 3, 4, 5, 6, 1],
-      items2: [1, 2, 3, 4, 5, 6, 1],
+      // items2: [1, 2, 3, 4, 5, 6, 1],
       followingList: null,
       followerList: null,
     }
