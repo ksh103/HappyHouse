e4fd842d6e20c8ddefaa2b95e0d4449527bb914e
Dae-Eon, Park <mem29238@gmail.com>
2021-11-23 20:47:24 +0900

    전화번호 칼럼 추가

--- server/src/main/java/com/ssafy/happyhouse/dto/CompanyDto.java
+++ server/src/main/java/com/ssafy/happyhouse/dto/CompanyDto.java
@@ -1,98 +1,24 @@
 package com.ssafy.happyhouse.dto;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class CompanyDto {
 	private int compSeq;
 	private String compId;
 	private String compName;
 	private String compPassword;
 	private String compEmail;
+	private String compPhone;
 	private String compAddress;
 	private LocalDateTime regDt;
 	private String compProfileimage;
 	private int code;
 	
-	public CompanyDto() {}
-	public CompanyDto(int compSeq, String compId, String compName, String compPassword, String compEmail,
-			String compAddress, String compProfileimage, int code) {
-		super();
-		this.compSeq = compSeq;
-		this.compId = compId;
-		this.compName = compName;
-		this.compPassword = compPassword;
-		this.compEmail = compEmail;
-		this.compAddress = compAddress;
-		this.compProfileimage = compProfileimage;
-		this.code = code;
-	}
-	
-	public int getCompSeq() {
-		return compSeq;
-	}
-	public void setCompSeq(int compSeq) {
-		this.compSeq = compSeq;
-	}
-	public String getCompId() {
-		return compId;
-	}
-	public void setCompId(String compId) {
-		this.compId = compId;
-	}
-	public String getCompName() {
-		return compName;
-	}
-	public void setCompName(String compName) {
-		this.compName = compName;
-	}
-	public String getCompPassword() {
-		return compPassword;
-	}
-	public void setCompPassword(String compPassword) {
-		this.compPassword = compPassword;
-	}
-	public String getCompEmail() {
-		return compEmail;
-	}
-	public void setCompEmail(String compEmail) {
-		this.compEmail = compEmail;
-	}
-	public String getCompAddress() {
-		return compAddress;
-	}
-	public void setCompAddress(String compAddress) {
-		this.compAddress = compAddress;
-	}
-	public LocalDateTime getRegDt() {
-		return regDt;
-	}
-	public void setRegDt(Date regDt) {
-		this.regDt = LocalDateTime.ofInstant(
-				regDt.toInstant(), ZoneId.systemDefault()
-		);
-	}
-	public String getCompProfileimage() {
-		return compProfileimage;
-	}
-	public void setCompProfileimage(String compProfileimage) {
-		this.compProfileimage = compProfileimage;
-	}
-	public int getCode() {
-		return code;
-	}
-	public void setCode(int code) {
-		this.code = code;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("CompanyDto [compSeq=").append(compSeq).append(", compId=").append(compId).append(", compName=")
-				.append(compName).append(", compPassword=").append(compPassword).append(", compEmail=")
-				.append(compEmail).append(", compAddress=").append(compAddress).append(", regDt=").append(regDt)
-				.append(", compProfileimage=").append(compProfileimage).append(", code=").append(code).append("]");
-		return builder.toString();
-	}
 }
--- server/src/main/java/com/ssafy/happyhouse/dto/UserDto.java
+++ server/src/main/java/com/ssafy/happyhouse/dto/UserDto.java
@@ -1,16 +1,22 @@
 package com.ssafy.happyhouse.dto;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class UserDto {
 	private int userSeq;
 	private String userId;
 	private String userName;
 	private String userPassword;
 	private String userEmail;
+	private String userPhone;
 	private LocalDateTime regDt;
 	private String userProfileimage;
 	private int code;
@@ -21,93 +27,4 @@ public class UserDto {
 	
 	private List<UserFileDto> fileList;
 	
-	public UserDto() {}
-	public UserDto(int userSeq, String userId, String userName, String userPassword, String userEmail, String userProfileimage, int code) {
-		this.userSeq = userSeq;
-		this.userId = userId;
-		this.userName = userName;
-		this.userPassword = userPassword;
-		this.userEmail = userEmail;
-		this.userProfileimage = userProfileimage;
-		this.code = code;
-	}
-	
-	public int getUserSeq() {
-		return userSeq;
-	}
-	public void setUserSeq(int userSeq) {
-		this.userSeq = userSeq;
-	}
-	public String getUserId() {
-		return userId;
-	}
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getUserPassword() {
-		return userPassword;
-	}
-	public void setUserPassword(String userPassword) {
-		this.userPassword = userPassword;
-	}
-	public String getUserEmail() {
-		return userEmail;
-	}
-	public void setUserEmail(String userEmail) {
-		this.userEmail = userEmail;
-	}
-	public LocalDateTime getRegDt() {
-		return regDt;
-	}
-	public void setRegDt(Date regDt) {
-		this.regDt = LocalDateTime.ofInstant(
-				regDt.toInstant(), ZoneId.systemDefault());
-	}
-	public String getUserProfileimage() {
-		return userProfileimage;
-	}
-	public void setUserProfileimage(String userProfileimage) {
-		this.userProfileimage = userProfileimage;
-	}
-	public int getCode() {
-		return code;
-	}
-	public void setCode(int code) {
-		this.code = code;
-	}
-	public boolean isSameUser() {
-		return sameUser;
-	}
-	public void setSameUser(boolean sameUser) {
-		this.sameUser = sameUser;
-	}
-	public List<UserFileDto> getFileList() {
-		return fileList;
-	}
-	public void setFileList(List<UserFileDto> fileList) {
-		this.fileList = fileList;
-	}
-	
-	public boolean isFriend() {
-		return friend;
-	}
-	public void setFriend(boolean friend) {
-		this.friend = friend;
-	}
-	@Override
-	public String toString() {
-		StringBuilder builder = new StringBuilder();
-		builder.append("UserDto [userSeq=").append(userSeq).append(", userId=").append(userId).append(", userName=")
-				.append(userName).append(", userPassword=").append(userPassword).append(", userEmail=")
-				.append(userEmail).append(", regDt=").append(regDt).append(", userProfileimage=")
-				.append(userProfileimage).append(", code=").append(code).append(", fileList=").append(fileList)
-				.append("]");
-		return builder.toString();
-	}
 }
