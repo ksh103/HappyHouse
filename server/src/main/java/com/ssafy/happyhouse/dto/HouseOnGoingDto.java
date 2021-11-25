a9dbe498e11fe5f496877a7a14e9fe3ad27fed9f
Dae-Eon, Park <mem29238@gmail.com>
2021-11-25 15:13:47 +0900

    쿼리에 받아올 데이터 추가

--- server/src/main/java/com/ssafy/happyhouse/dto/HouseOnGoingDto.java
+++ server/src/main/java/com/ssafy/happyhouse/dto/HouseOnGoingDto.java
@@ -1,12 +1,14 @@
 package com.ssafy.happyhouse.dto;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
+@Getter
+@Setter
 @ToString
 public class HouseOnGoingDto {
 	int ongoingId; 
@@ -15,6 +17,8 @@ public class HouseOnGoingDto {
 	String AptName;
 	String compName;
 	String compAddress;
+	String compEmail;
+	String compPhone;
 	String title; // 매물 이름
 	String content; // 매물 내용
 	String dealAmount; // 가격
@@ -33,115 +37,4 @@ public class HouseOnGoingDto {
 	
 	private List<HouseOnGoingFileDto> fileList;
 	
-	public int getOngoingId() {
-		return ongoingId;
-	}
-	public void setOngoingId(int ongoingId) {
-		this.ongoingId = ongoingId;
-	}
-	public int getHouseNo() {
-		return houseNo;
-	}
-	public void setHouseNo(int houseNo) {
-		this.houseNo = houseNo;
-	}
-	public int getCompSeq() {
-		return compSeq;
-	}
-	public void setCompSeq(int compSeq) {
-		this.compSeq = compSeq;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public LocalDateTime getRegDt() {
-		return regDt;
-	}
-	public void setRegDt(Date regDt) {
-		this.regDt = LocalDateTime.ofInstant(
-				regDt.toInstant(), ZoneId.systemDefault()
-		);
-	}
-	public String getDealAmount() {
-		return dealAmount;
-	}
-	public void setDealAmount(String dealAmount) {
-		this.dealAmount = dealAmount;
-	}
-	public int getFloor() {
-		return floor;
-	}
-	public void setFloor(int floor) {
-		this.floor = floor;
-	}
-	public int getArea() {
-		return area;
-	}
-	public void setArea(int area) {
-		this.area = area;
-	}
-	public String getDirection() {
-		return direction;
-	}
-	public void setDirection(String direction) {
-		this.direction = direction;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getFee() {
-		return fee;
-	}
-	public void setFee(String fee) {
-		this.fee = fee;
-	}
-	public int getRoom() {
-		return room;
-	}
-	public void setRoom(int room) {
-		this.room = room;
-	}
-	public int getBathroom() {
-		return bathroom;
-	}
-	public void setBathroom(int bathroom) {
-		this.bathroom = bathroom;
-	}
-	public boolean isSameUser() {
-		return sameUser;
-	}
-	public void setSameUser(boolean sameUser) {
-		this.sameUser = sameUser;
-	}
-	
-	public boolean isBookmark() {
-		return bookmark;
-	}
-	public void setBookmark(boolean bookmark) {
-		this.bookmark = bookmark;
-	}
-	public List<HouseOnGoingFileDto> getFileList() {
-		return fileList;
-	}
-	public void setFileList(List<HouseOnGoingFileDto> fileList) {
-		this.fileList = fileList;
-	}
-	public String getCompName() {
-		return compName;
-	}
-	public void setCompName(String compName) {
-		this.compName = compName;
-	}
 }
